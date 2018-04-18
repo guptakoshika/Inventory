5ef4171ab15fee5e06fa6567a92e4ebe9f7771c4
Koshika Gupta <koshika1464.cse16@chitkara.edu.in>
2018-04-18 18:02:42 +0530

    Update InventoryAdapter.java

--- mygrocerystore/app/src/main/java/com/example/android/mygrocerystore/InventoryAdapter.java
+++ mygrocerystore/app/src/main/java/com/example/android/mygrocerystore/InventoryAdapter.java
@@ -2,6 +2,7 @@ package com.example.android.mygrocerystore;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Koshika Gupta on 31-03-2018.
@@ -33,39 +35,25 @@ public class InventoryAdapter extends CursorAdapter {
         TextView nameTextView = view.findViewById(R.id.name);
         TextView summaryTextView =  view.findViewById(R.id.price);
         TextView QuantityTextView = view.findViewById(R.id.quantity);
-     //   ImageView image = (ImageView) view.findViewById(R.id.image_view1);
+        ImageView image = (ImageView) view.findViewById(R.id.image_view1);
 
         // Find the columns of pet attributes that we're interested in
         int nameColumnIndex = cursor.getColumnIndex(Inventorycontract.newItem.COLUMN_ITEM_NAME);
-        int breedColumnIndex = cursor.getColumnIndex(Inventorycontract.newItem.COLUMN_ITEM_PRICE);
+        int priceColumnIndex = cursor.getColumnIndex(Inventorycontract.newItem.COLUMN_ITEM_PRICE);
         int quantityColumnIndex = cursor.getColumnIndex(Inventorycontract.newItem.COLUMN_ITEM_QUANTITY);
+        int imageColumnIndex = cursor.getColumnIndex(Inventorycontract.newItem.COLUMN_IMAGE);
 
-       // image.setImageURI(Uri.parse(cursor.getString(cursor.getColumnIndex(Inventorycontract.newItem.COLUMN_IMAGE))));
-
-        // Read the pet attributes from the Cursor for the current pet
+        // Read the pet attributes from the Cursor for the current item
         String itemName = cursor.getString(nameColumnIndex);
-        String itemPrice = cursor.getString(breedColumnIndex);
+        String itemPrice = cursor.getString(priceColumnIndex);
         String itemQuantity = cursor.getString(quantityColumnIndex);
+        String imageView = cursor.getString(imageColumnIndex);
 
-
-        // Update the TextViews with the attributes for the current pet
+        // Update the TextViews with the attributes for the current item
         nameTextView.setText(itemName);
         summaryTextView.setText(itemPrice);
         QuantityTextView.setText(itemQuantity);
-        final long id = cursor.getLong(cursor.getColumnIndex(Inventorycontract.newItem._ID));
-
-       /* view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                activity.clickOnViewItem(id);
-            }
-        });
-        sale.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                activity.clickOnSale(id,
-                        QuantityTextView);
-            }
-        });*/
+        image.setImageURI(Uri.parse(imageView));
+       // image.setImageResource(imageView);
     }
 }
